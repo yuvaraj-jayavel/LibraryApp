b8418a6f68a327abbc3b35bec2af12afbf55fad9
Yuvaraj J <jyuvaraj03@gmail.com>
2021-11-06 15:12:43 +0530

    Calculate book rental's due_by and fine

--- app/models/book_rental.rb
+++ app/models/book_rental.rb
@@ -1,4 +1,7 @@
 class BookRental < ApplicationRecord
+  DUE_BY_DAYS = 15
+  FINE_PER_DAY = 1
+
   belongs_to :book
   belongs_to :member
 
@@ -12,6 +15,22 @@ class BookRental < ApplicationRecord
 
   def borrowed_book
     "#{book.name} ##{book.id}"
+  end
+
+  def returned?
+    !returned_on.nil?
+  end
+
+  def due_by
+    issued_on + DUE_BY_DAYS.days
+  end
+
+  def fine
+    if returned? || due_by >= Date.today
+      0
+    else
+      ((Date.today - due_by) * FINE_PER_DAY)
+    end
   end
 
   private
--- test/models/book_rental_test.rb
+++ test/models/book_rental_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class BookRentalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'due_by should be equal to DUE_BY_DAYS + issued_on date' do
+    issued_on = Date.today
+    book_rental = BookRental.new(
+      book: books(:unborrowed),
+      member: members(:johnny),
+      issued_on: issued_on
+    )
+
+    assert_equal issued_on + BookRental::DUE_BY_DAYS, book_rental.due_by
+  end
+
+  test 'fine should be zero if the book has already been returned' do
+    book_rental = book_rentals(:returned)
+    assert_equal 0, book_rental.fine
+  end
+
+  test 'fine should be zero if due_by is today or in future' do
+    book_rental = BookRental.new(
+      book: books(:unborrowed),
+      member: members(:johnny),
+      issued_on: Date.today # this ensures due_by will be in the future
+    )
+    assert_operator book_rental.due_by, :>, Date.today
+    assert_equal 0, book_rental.fine
+
+    book_rental.issued_on = Date.today - BookRental::DUE_BY_DAYS
+    assert_equal Date.today, book_rental.due_by
+    assert_equal 0, book_rental.fine
+  end
+
+  test 'fine should be equal to number of days elapsed after the book is due multiplied by FINE_PER_DAY' do
+    days_elapsed_after_due_by = 5
+    issued_on = Date.today - (BookRental::DUE_BY_DAYS + days_elapsed_after_due_by)
+    book_rental = BookRental.new(
+      book: books(:unborrowed),
+      member: members(:johnny),
+      issued_on: issued_on
+    )
+    assert_equal Date.today - days_elapsed_after_due_by, book_rental.due_by
+
+    assert_equal days_elapsed_after_due_by * BookRental::FINE_PER_DAY, book_rental.fine
+  end
 end
