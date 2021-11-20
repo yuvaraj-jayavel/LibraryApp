a93da3e9bc58c0594cf21eb2be251f36f4968cf6
Yuvaraj J <jyuvaraj03@gmail.com>
2021-11-20 23:17:16 +0530

    Add activerecord model translations

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module LibraryApp
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.available_locales = %i[en ta]
     config.action_dispatch.rescue_responses['Pundit::NotAuthorizedError'] = :forbidden
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
